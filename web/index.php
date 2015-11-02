457f039a435ca1558790722e0c1ca5406123b421
SnowyYANG <snowyyang@outlook.com>
2015-11-02 23:27:36 +0800

    record requests

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
 <?php
 
+require_once 'private/data.php';
+$mysqli = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+date_default_timezone_set('Asia/Hong_Kong');
+$time = date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
+$uri = mysqli_real_escape_string($mysqli, $_SERVER['REQUEST_URI']);
+$ip = mysqli_real_escape_string($mysqli, $_SERVER['REMOTE_ADDR']);
+$browser = mysqli_real_escape_string($mysqli, $_SERVER['HTTP_USER_AGENT']);
+mysqli_query($mysqli, "INSERT requests (time, uri, ip, browser) VALUES ('$time', '$uri', '$ip', '$browser')", MYSQLI_USE_RESULT);
+
 if ($q = $_GET['q']) {
     $q = explode('/', $q);
     switch ($q[1]) {
@@ -12,6 +22,8 @@ if ($q = $_GET['q']) {
 if (!isset($router)) $router = 'homepage';
 
 require 'private/'.$router.'.php';
+
+mysqli_close($mysqli);
 ?>
 <!DOCTYPE html>
 <!--
