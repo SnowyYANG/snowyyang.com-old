d8d958afecd185beee1dd71aeb36d3e5e31ef55b
SnowyYANG <SnowyYANG@outlook.com>
2018-01-06 23:03:42 +0800

    梦幻拉比统计 about

--- private/about.php
+++ private/about.php
@@ -17,12 +17,20 @@ Alice Snowy Fei YANG<br>
 <br>
 出生于安徽省芜湖市<br>
 常年在香港深居简出<br>
-<span style="text-decoration:line-through">事业</span>感情均已稳定 <span style="text-decoration:line-through">谢绝各类猎头</span><br>
-人生梦想成为全能天才美少女！<br>
-实际追求吃了睡睡了吃Zzz..<br>
+事业感情均已稳定 谢绝各类猎头<br>
+人生梦想成为全能天才美少女<br>
+实际追求吃吃睡睡<br>
 <br>
-洛奇：冰之妖夜（玛丽服务器）<br>
+Aldnoah.Zero 斯雷因双公主党<br>
+白色相簿2 冬马TE结局党 千晶角色党<br>
+洛奇 生活系玩家<br>
+精灵宝可梦 孵蛋爱好者<br>
+符文工房4 亚瑟控<br>
+霹雳布袋戏 死国粉<br>
+<br>
+LinkedIn: <a target="_blank" href="https://www.linkedin.com/in/alicefeiyang/">Alice Fei YANG</a><br>
 GitHub: <a target="_blank" href="https://github.com/SnowyYANG">SnowyYANG</a><br>
+洛奇：冰之妖夜（国服全服 大号在玛丽服务器）<br>
 微博：<a target="_blank" href="http://weibo.com/AliceFYANG">@大正8年8月的东京Yurika</a><br>
 Twitter: <a target="_blank" href="https://twitter.com/SnowyHAMUSUTAA">@SnowyHAMUSUTAA</a><br>
 百度贴吧：<a target="_blank" href="http://tieba.baidu.com/home/main?un=%E8%8B%A5%E9%9B%AA%E7%88%B1%E4%B8%BD%E4%B8%9D">若雪爱丽丝</a><br>
