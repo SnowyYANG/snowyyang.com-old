6b2a7ed1a0dfc09064b92482109e5a08c574b196
SnowyYANG <SnowyYANG@outlook.com>
2018-01-09 01:49:40 +0800

    mabicolor

--- private/mc.php
+++ private/mc.php
@@ -7,37 +7,73 @@
 
 $title = '洛奇普染颜色大全';
 
+$style = '
+.mcblock {
+    text-align:center;
+    vertical-align: middle;
+    padding-top:0.5em;
+    width:10em;
+    height:2em;
+    display:inline-block;
+}';
+
 if ($_REQUEST['s']) {
-    $s = $mysqli->escape_string($_REQUEST['s']);
+    $s = $mysqli->escape_string($_SERVER['QUERY_STRING']);
     $mysqli->query("INSERT mc_s(ip,s) VALUES('$ip','$s')");
 }
 
 function view() {
 ?>
 <h1>洛奇普通染色颜色代码大全</h1>
-<p>by 冰之妖夜（玛丽服务器）</p>
-<p>欢迎使用洛奇普通染色颜色代码大全。请使用Firefox浏览器打开本网页，IE浏览器似乎不能正常使用。<span style="color:red">年内将更新</span>加入留言、推荐颜色等功能。<br>
-目前只有布染全彩，共13537种颜色，皮革、淡彩、浓彩的数据目前没有（因为我太懒了）。<br>
-下载所有颜色列表：<a href="<?php echo SITE;?>mc.txt">mc.txt</a>，<a href="<?php echo SITE; ?>mc.xlsx">mc.xlsx</a>。</p>
-
-<form id="f"><input name="s" style="width:80%" type="text" placeholder="#008000(100) R&lt;64 B&lt;64"><input type="submit" value="搜索"></form><br>
-<div>“#十六进制颜色代码(数量)”查找指定数量的最相似颜色，注意请使用半角#()，如“#FFFF00(100)”搜索近似的100种亮黄色；<br>
-“R/G/B大于小于等于号十进制数值”查找符合要求的颜色，注意字母大写符号半角单条语句不要有空格，如“G=0 B=0”搜索纯红色；<br>
-混合使用以上查找，直接用空格隔开，如“#008000(100) R&lt;64 B&lt;64”搜索近似纯绿的100种中绿色。<br>
-太多人不喜欢打“#”，现在可以省略这个符号了。颜色对比算法有限，有时搜出来的颜色怪怪的，不怕浏览器卡的话建议#XXXXXX(500)。<br>
-推荐纸娃娃网址：<a href="http://labo.erinn.biz/cs/index.php?action=changeFramework">http://labo.erinn.biz/cs/index.php?action=changeFramework<a><br><br>
-</div>
+<p>by 冰之妖夜（国服全服 日常在玛丽服务器）</p>
+<p>欢迎使用洛奇普通染色颜色代码大全，这里可以搜索普染和金属普染能染出的精确颜色代码。<br>
+<span style="color:red">本页面最近几天正在更新中，内容可能不稳定。</span>下一版更新：推荐颜色、留言板。<br>请使用Firefox、Chrome浏览器打开本网页，IE浏览器目前不能正常使用。<br>
+<br>
+<table>
+    <tr>
+    <td><img id="dye" src="<?php echo SITE;?>mabicolor/cloth.png"></td>
+    <td style="margin-left:1em">
+        <form>
+            <input type="radio" name="c" value="cloth" checked/>全彩
+            <input type="radio" name="c" value="cloth_bright" />淡彩
+            <input type="radio" name="c" value="leather" />皮革
+            <input type="radio" name="c" value="metal" />金属
+            <input type="radio" name="c" value="weapon" />武器
+            <input type="radio" name="c" value="silk" />丝绸
+            <br>
+            <input name="s" style="width:25.3em" type="text" placeholder="#008000 R&lt;64 B&lt;64">
+            <input type="submit" value="搜索">
+        </form>
+    <p>“#十六进制颜色代码”查找最相似颜色，如“#FFFF00”搜索近似的亮黄色；<br>
+    “R/G/B大于小于等于号十进制数值”筛选颜色，字母大写符号半角不要有空格，如“G=0 B=0”搜索纯红色；<br>
+    混合使用以上查找，用空格隔开，如“#008000 R&lt;64 B&lt;64”搜索中绿色。<br>
+    颜色对比算法正在调整中。<br>
+    <br>
+    推荐染色助手：<a href="http://www.yydzh.com/read.php?tid=1402600">http://www.yydzh.com/read.php?tid=1402600</a><br>
+    推荐纸娃娃模拟器：<a href="http://labo.erinn.biz/cs/index.php?action=changeFramework">http://labo.erinn.biz/cs/index.php?action=changeFramework</a><br></p>
+    </td>
+    </tr>
+</table>
 <div id="r"></div>
-<script src="<?php echo SITE;?>mccolors.js"></script>
+<script src="<?php echo SITE;?>mabicolor/cloth.js"></script>
+<script src="<?php echo SITE;?>mabicolor/cloth_bright.js"></script>
+<script src="<?php echo SITE;?>mabicolor/leather.js"></script>
+<script src="<?php echo SITE;?>mabicolor/metal.js"></script>
+<script src="<?php echo SITE;?>mabicolor/silk.js"></script>
+<script src="<?php echo SITE;?>mabicolor/weapon.js"></script>
 <script>
-    function compare(a,b) {
-        return a[4]-b[4];
-    }
-    
-    function colorBlock(r,g,b,color, str) {
-        var fc = 0.2126*r + 0.7152*g + 0.0722*b>128 ? '#000000' : '#FFFFFF';
-        return '<div style="text-align:center;vertical-align: middle;padding-top:0.5em;width:10em;height:2em;display:inline-block;background:'+color+';color:'+fc+'">'+str+'</div>';
-    }
+function compare(a,b) {
+    return a[4]-b[4];
+}
+
+function RGB2HTML(red, green, blue) {
+    var decColor =0x1000000 | blue | (green << 8) | (red << 16);
+    return '#'+decColor.toString(16).substr(1);
+}
+function colorBlock(r,g,b,color,str) {
+    var fc = 0.2126*r + 0.7152*g + 0.0722*b>128 ? '#000000' : '#FFFFFF';
+    return '<div class="mcblock" style="background:'+color+';color:'+fc+'">'+str+'</div>';
+}
     
 function rgb2lab(rgb){
   var r = rgb[0] / 255,
@@ -64,76 +100,107 @@ function rgb2lab(rgb){
 // https://github.com/THEjoezack/ColorMine/blob/master/ColorMine/ColorSpaces/Comparisons/Cie94Comparison.cs
 
 function deltaE(labA, labB){
-  var deltaL = labA[0] - labB[0];
-  var deltaA = labA[1] - labB[1];
-  var deltaB = labA[2] - labB[2];
-  var c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
-  var c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
-  var deltaC = c1 - c2;
-  var deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
-  deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
-  var sc = 1.0 + 0.045 * c1;
-  var sh = 1.0 + 0.015 * c1;
-  var deltaLKlsl = deltaL / (1.0);
-  var deltaCkcsc = deltaC / (sc);
-  var deltaHkhsh = deltaH / (sh);
-  var i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
-  return i < 0 ? 0 : Math.sqrt(i);
+    var deltaL = labA[0] - labB[0];
+    var deltaA = labA[1] - labB[1];
+    var deltaB = labA[2] - labB[2];
+    var c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+    var c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+    var deltaC = c1 - c2;
+    var deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+    deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+    var sc = 1.0 + 0.045 * c1;
+    var sh = 1.0 + 0.015 * c1;
+    var deltaLKlsl = deltaL / (1.0);
+    var deltaCkcsc = deltaC / (sc);
+    var deltaHkhsh = deltaH / (sh);
+    var i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+    return i < 0 ? 0 : Math.sqrt(i);
 }
 
-    document.getElementById('f').onsubmit = function() {
-		if (!this.s.value) return false;
-        var xhr = new XMLHttpRequest();
-        if (xhr) {
-            xhr.open('GET','<?php echo SITE;?>mc?s='+encodeURIComponent(this.s.value));
-            xhr.send();
-        }
-        var ss = this.s.value.split(' ');
-        var results = [];
-        for (var c of colors) {
-            var match = true;
-            for (var s of ss)
-                if (s[0] === 'R' || s[0] === 'G' || s[0] === 'B') {
-                    num = parseInt(s.substring(2));
-                    if (s[0] === 'R') a = c[0];
-                    else if (s[0] === 'G') a = c[1];
-                    else a = c[2];
-                    match &= s[1] === '=' ? a === num : s[1] === '<' ? a < num : a > num;
-                }
-            if (match) {
-                results.push(c);
-            }
-        }
-        for (var s of ss) {
-			if (s[0] !== '#' && s.substring(0,6).match(/^[0-9a-f]{6}/i)) s='#'+s;
-            if (s[0] === '#') {
-				if (s.length===7) s=s+'(100)';
-                var R = parseInt("0x" + s.substring(1,3));
-                var G = parseInt("0x" + s.substring(3,5));
-                var B = parseInt("0x" + s.substring(5,7));
-                var S = s.substring(0,7);
-                var N = parseInt(s.substring(8,s.length-1));
-                for (var c of colors) {
-					c[4] = 2*Math.pow(c[0]-R,2)+4*Math.pow(c[1]-G,2)+3*Math.pow(c[2]-B,2);
-					//c[4]=deltaE(rgb2lab(c),rgb2lab([R,G,B]));
-				}
-                results.sort(compare);
-                break;
-            }
-        }
-        var r = document.getElementById('r');
-        r.innerHTML = '';
-        if (S) r.innerHTML += colorBlock(R,G,B,S,'目标颜色');
-		if (!N) N=100;
-        if (results.length === 0) r.innerHTML += '<div>没有找到任何颜色</div>';
-        else {
-            for (var i=0;i<results.length;++i) {
-                if (i>=N && results[i][4] !== results[N-1][4]) break;
-                r.innerHTML += colorBlock(results[i][0],results[i][1],results[i][2],results[i][3],results[i][3]);
-            }
-        }
-        return false;
+var radios = document.forms[0].c;
+for(var radio of radios) {
+    radio.onchange = function() {
+        document.getElementById('dye').src = '<?php echo SITE;?>mabicolor/'+this.value+'.png';
     };
+}
+
+var results = [];
+document.forms[0].onsubmit = function() {
+    if (!this.s.value) return false;
+    var xhr = new XMLHttpRequest();
+    if (xhr) {
+        xhr.open('GET','<?php echo SITE;?>mc?c='+encodeURIComponent(this.c.value)+'&s='+encodeURIComponent(this.s.value));
+        xhr.send();
+    }
+    var ss = this.s.value.split(' ');
+    results = [];
+    var colors;
+    switch(document.forms[0].c.value) {
+        case 'cloth': colors = cloth; break;
+        case 'cloth_bright': colors = cloth_bright; break;
+        case 'leather': colors = leather; break;
+        case 'metal': colors = metal; break;
+        case 'silk': colors = silk; break;
+        case 'weapon': colors = weapon; break;
+        default: colors = [];
+    }
+    for (var c of colors) {
+        var match = true;
+        for (var s of ss)
+            if (s[0] === 'R' || s[0] === 'G' || s[0] === 'B') {
+                num = parseInt(s.substring(2));
+                if (s[0] === 'R') a = c[0];
+                else if (s[0] === 'G') a = c[1];
+                else a = c[2];
+                match &= s[1] === '=' ? a === num : s[1] === '<' ? a < num : a > num;
+            }
+        if (match) {
+            results.push(c);
+        }
+    }
+    for (var s of ss) {
+        if (s[0] !== '#' && s.substring(0,6).match(/^[0-9a-f]{6}/i)) s='#'+s;
+        if (s[0] === '#') {
+            var R = parseInt("0x" + s.substring(1,3));
+            var G = parseInt("0x" + s.substring(3,5));
+            var B = parseInt("0x" + s.substring(5,7));
+            var S = s;
+            for (var c of results) {
+                c[4] = 2*Math.pow(c[0]-R,2)+4*Math.pow(c[1]-G,2)+3*Math.pow(c[2]-B,2);
+                //c[4]=deltaE(rgb2lab(c),rgb2lab([R,G,B]));
+            }
+            results.sort(compare);
+            break;
+        }
+    }
+    var r = [];
+    if (S) r.push(colorBlock(R,G,B,S,'目标颜色'));
+    if (results.length === 0) r.push('<div>没有找到任何颜色</div>');
+    else {
+        var i = 0;
+        while (i < results.length) {
+            if (i >= 100 && results[i][4] !== results[i-1][4]) break;
+            var code = RGB2HTML(results[i][0],results[i][1],results[i][2]);
+            r.push(colorBlock(results[i][0],results[i][1],results[i][2],code,code));
+            ++i;
+        }
+        if (i < results.length) r.push('<div id="more" onclick="morecolor(' + i + ')" class="mcblock" style="background:#f0f0f0;color:#6699cc;cursor:pointer">更多颜色</div>');
+        document.getElementById('r').innerHTML = r.join('');
+    }
+    return false;
+};
+
+function morecolor(num) {
+    var more = document.getElementById('more');
+    var r = [];
+    for (var i = num; i < results.length; ++i) {
+        if (i >= num + 100 && results[i][4] !== results[i-1][4]) break;
+        var code = RGB2HTML(results[i][0],results[i][1],results[i][2]);
+        r.push(colorBlock(results[i][0],results[i][1],results[i][2],code,code));
+    }
+    if (i < results.length) r.push('<div id="more" onclick="morecolor(' + i + ')" class="mcblock" style="background:#f0f0f0;color:#6699cc;cursor:pointer">更多颜色</div>');
+    more.outerHTML = r.join('');
+}
 </script>
 <?php
 }
