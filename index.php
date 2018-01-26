57c58614273befb8e8436cd2238e3616ee4c6f66
SnowyYANG <SnowyYANG@outlook.com>
2018-01-27 01:07:18 +0800

    ..

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ if ($q = $_GET['q']) {
     switch ($q[1]) {
         case 'mc':
         case 'about':
+        case 'kazeyuki816':
             $router = $q[1];
             break;
         case 'rfwiki':
--- private/about.php
+++ private/about.php
@@ -21,13 +21,6 @@ Alice Snowy Fei YANG<br>
 人生梦想成为全能天才美少女<br>
 实际追求吃吃睡睡<br>
 <br>
-Aldnoah.Zero 斯雷因双公主党<br>
-白色相簿2 冬马TE结局党 千晶角色党<br>
-洛奇 生活系玩家<br>
-精灵宝可梦 孵蛋爱好者<br>
-符文工房4 亚瑟控<br>
-霹雳布袋戏 死国粉<br>
-<br>
 八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>
 经风经雨声不毁 历霜历雪更妖娆<br>
 机关算尽不误命 露财露色也平安<br>
@@ -35,7 +28,7 @@ Aldnoah.Zero 斯雷因双公主党<br>
 <br>
 LinkedIn: <a target="_blank" href="https://www.linkedin.com/in/alicefeiyang/">Alice Fei YANG</a><br>
 GitHub: <a target="_blank" href="https://github.com/SnowyYANG">SnowyYANG</a><br>
-洛奇：冰之妖夜（国服全服 大号在玛丽服务器）<br>
+洛奇：冰之妖夜（国服全服）<br>
 微博：<a target="_blank" href="http://weibo.com/AliceFYANG">@大正8年8月的东京Yurika</a><br>
 Twitter: <a target="_blank" href="https://twitter.com/SnowyHAMUSUTAA">@SnowyHAMUSUTAA</a><br>
 百度贴吧：<a target="_blank" href="http://tieba.baidu.com/home/main?un=%E8%8B%A5%E9%9B%AA%E7%88%B1%E4%B8%BD%E4%B8%9D">若雪爱丽丝</a><br>
