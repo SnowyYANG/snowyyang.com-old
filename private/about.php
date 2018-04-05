319edba1d18dea9894f67c4e0653eccb8c7f2459
SnowyYANG <SnowyYANG@outlook.com>
2018-04-05 09:46:13 +0800

    dynamic 诗号

--- private/about.php
+++ private/about.php
@@ -22,10 +22,10 @@ Alice Snowy Fei YANG<br>
 实际追求吃吃睡睡<br>
 <br>
 <img src="/kazeyuki816.jpg"><br>
-八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>
-经风|枫经雨生不毁|悔 若霜若雪尽妖娆<br>
-机关算尽不误命 露才|财露色也平安<br>
-耐得春宵萧寒夜 平步青云又一朝 | 笼鸟不出妖恋色 冰心难解夜梦来​<br>
+<span id="sh">八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>
+经风经雨生不毁 若霜若雪尽妖娆<br>
+机关算尽不误命 露财露色也平安<br>
+耐得春宵萧寒夜 平步青云又一朝​<br></span>
 <br>
 LinkedIn: <a target="_blank" href="https://www.linkedin.com/in/alicefeiyang/">Alice Fei YANG</a><br>
 GitHub: <a target="_blank" href="https://github.com/SnowyYANG">SnowyYANG</a><br>
@@ -33,5 +33,13 @@ GitHub: <a target="_blank" href="https://github.com/SnowyYANG">SnowyYANG</a><br>
 精灵宝可梦：大正8年8月的东京百合花<br>
 百度贴吧：<a target="_blank" href="http://tieba.baidu.com/home/main?un=%E8%8B%A5%E9%9B%AA%E7%88%B1%E4%B8%BD%E4%B8%9D">若雪爱丽丝</a><br>
 </p>
+<script>
+document.getElementById('sh').onmouseenter = function() {
+	sh.innerHTML = '八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>经枫经雨生不悔 历霜历雪尽妖娆<br>机关算尽不误命 露才露色也平安<br>笼鸟不出妖恋色 冰心难解夜梦来​<br>';
+}
+document.getElementById('sh').onmouseleave = function() {
+	sh.innerHTML = '八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>经风经雨生不毁 若霜若雪竞妖娆<br>机关算尽不误命 露财露色也平安<br>耐得春宵萧寒夜 平步青云又一朝​<br>';
+}
+</script>
 <?php
 }
