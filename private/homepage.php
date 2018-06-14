d713990938104cbec86c3dbf9b385e05ec332103
SnowyYANG <SnowyYANG@outlook.com>
2018-06-14 22:46:18 +0800

    emoji to text

--- private/homepage.php
+++ private/homepage.php
@@ -9,9 +9,9 @@ function view() {
 ?>
 <p>
 	欢迎来到Snowy的雪亚幻想境地（原名少女工房）<br>
-	❄ <a href="/rfwiki">符文工房中文百科</a><br>
-	❄ <a href="/mc">洛奇普通染色颜色代码大全</a><br>
-	❄ <a href="/dreamcage">雪鸟幻梦之笼</a> - 原创小说<br>
+	❄&#65038; <a href="/rfwiki">符文工房中文百科</a><br>
+	❄&#65038; <a href="/mc">洛奇普通染色颜色代码大全</a><br>
+	❄&#65038; <a href="/dreamcage">雪鸟幻梦之笼</a> - 原创小说<br>
 </p>
 <p>
   关于<a href="/about">Snowy</a>
