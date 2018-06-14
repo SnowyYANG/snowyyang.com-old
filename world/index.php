31b9e555555446385c0401cdd9b377159a600661
SnowyYANG <SnowyYANG@outlook.com>
2018-06-14 21:08:48 +0800

    world login ui

--- world/index.php
+++ world/index.php
@@ -33,20 +33,13 @@ mysqli_query($mysqli, "INSERT world_requests (time, uri, referer, ip, browser) V
         </style>
     </head>
     <body>
-        <h1>时与空与梦的旅人</h1>
-        <h2>序章 所谓游戏是在追求数值吗？</h2>
-        <p>
-            如今的很多手游，可能是一种赌博机。<br>
-            “我再去买几个水晶，你乖乖的呆在卡池里，不要走动。”<br>
-            这内在的数值，是一个迷一样的概率。<br>
-            我们这个游戏，大约不会有类似的模式，但它也有一个迷一样的概率——<br>
-            跳票概率<br>
-            当前跳票概率：<span style="color:blue">100%</span><br>
-            游戏做完开服后我会给注册用户的QQ邮箱发送邮件（有共同群的会临时对话通知，工期无法预计）。
-        </p>
-        <footer>
-            by 若雪爱丽丝（QQ 368487858）<br>
-            for 时与空与梦的旅人
-        </footer>
+		<div style="width:32em; horizontal-align:middle">
+			<form>
+				<input placeholder="用户名"><br>
+				<input placeholder="密码"><br>
+				<input type="submit" name="login" value="登陆">
+				<input type="submit" name="register" value="注册">
+			</form>
+		</div>
     </body>
 </html>
