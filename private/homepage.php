515da76880863323476a905a896eb4abd4c4e7f9
SnowyYANG <SnowyYANG@outlook.com>
2018-06-14 21:40:36 +0800

    rename site

--- private/homepage.php
+++ private/homepage.php
@@ -10,7 +10,7 @@ $title = 'Snowy';
 function view() {
 ?>
 <p>
-  欢迎来到Snowy的少女工房<br>
+  欢迎来到Snowy的雪亚幻想境地（原名少女工房）<br>
   <a href="/rfwiki">符文工房中文百科</a><br>
   <a href="/mc">洛奇普染颜色大全</a><br>
   <a href="/about">关于Snowy</a>
