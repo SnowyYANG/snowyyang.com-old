bbbafc987c21a3c44969787d4ffb091b2f8844ea
SnowyYANG <SnowyYANG@outlook.com>
2019-01-12 21:53:25 +0800

    modify homepage

--- private/homepage.php
+++ private/homepage.php
@@ -10,9 +10,9 @@ function view() {
 <p>
 	欢迎来到Snowy的雪亚幻想阵地<br><br>
 	❄&#65038; <a href="/rfwiki">符文工房中文百科</a><br>
-	❄&#65038; <a href="/mabiwiki">洛奇wiki</a><br>
-	❄&#65038; <a href="/mc">洛奇普通染色颜色代码大全</a><br>
-	❄&#65038; <a href="/mrp">洛奇梦幻拉比最终宝箱出物统计</a><br>
+	❄&#65038; <a href="/mabiwiki">洛奇wiki</a>（建设中）<br>
+	&emsp;❄&#65038; <a href="/mc">洛奇普通染色颜色代码大全</a><br>
+	&emsp;❄&#65038; <a href="/mrp">洛奇梦幻拉比最终宝箱出物统计</a><br>
 	<br>
 	个人小说<br>
 	❄&#65038; <a href="/dreamcage">雪鸟幻梦之笼</a><br>
