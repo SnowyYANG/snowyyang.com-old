169b53937753f85860abe7b6742b7b7ce2611f85
SnowyYANG <SnowyYANG@outlook.com>
2019-01-13 19:31:29 +0800

    minor fix

--- mabiwiki/index.php
+++ mabiwiki/index.php
@@ -1,3 +1,7 @@
+<?php 
+    if (($q = substr($_GET['q'],1)) && $q!=='index' && preg_match("/^[a-zA-Z0-9]+$/",$q) && @include("$q.php")) ;
+    else require 'homepage.php';
+?>
 <!DOCTYPE html>
 <!--
 by Snowy YANG
@@ -6,9 +10,8 @@ for http://snowy.asia/mabiwiki
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>洛奇wiki</title>
+        <title><?php echo $h2;?> - 洛奇wiki</title>
         <base href="/mabiwiki/">
-        <link href="favicon.ico" rel="shortcut icon"/>
         <link href="main.css" type="text/css" rel="stylesheet"/>
     </head>
     <body style="background:#316745">
@@ -70,8 +73,6 @@ for http://snowy.asia/mabiwiki
             </nav>
             <div style="display:flex;flex-direction:column;flex-grow:8;padding-right:2rem">
                 <main style="flex-grow:99;margin:0 1rem;word-break:break-all;width:100%;max-width:100%"><?php
-                    if (($q = substr($_GET['q'],1)) && $q!=='index' && preg_match("/^[a-zA-Z0-9]+$/",$q) && @include("$q.php")) ;
-                    else require 'homepage.php';
                     echo "<h2>$h2 <nav>$nav</nav></h2>";
                     if ($unf) echo '<div class="notice">（这个页面尚未完工）</div>';
                     mainview();
