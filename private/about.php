040265312a25ae20d3c2b1d4322e2cc98404cfb9
Snowy YANG <snowyyang@outlook.com>
2021-04-07 03:36:19 +0800

    通灵姬重命名 linkedin链接修改 备份nginx配置

--- private/about.php
+++ private/about.php
@@ -21,7 +21,7 @@ function view() {
 <span onmouseenter="yin(this)" onmouseleave="yang(this)">八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>经风经雨生不毁 历霜历雪尽妖娆<br>机关算尽不误命 露财露色也平安<br>耐得春宵萧寒夜 平步青云又一朝</span>​<br>
 <br>
 E-mail: <b>snowyyang(a)outlook.com</b><br>
-LinkedIn: <a target="_blank" href="https://www.linkedin.com/in/alicefeiyang/">Fei YANG</a><br>
+LinkedIn: <a target="_blank" href="https://www.linkedin.com/in/snowyyang/">Fei YANG</a><br>
 GitHub: <a target="_blank" href="https://github.com/SnowyYANG">SnowyYANG</a><br>
 豆瓣：<a target="_blank" href="https://www.douban.com/people/snowyyang/">Snowy</a><br>
 Pixiv：<a target="_blank" href="https://www.pixiv.net/member.php?id=15972970">大正８年８月の東京ユリカ</a><br>
--- private/homepage.php
+++ private/homepage.php
@@ -14,9 +14,10 @@ function view() {
 	❄&#65038; 洛奇Mabinogi<br>
 	&emsp;❄&#65038; <a href="/mc">普通染色颜色代码大全</a><br>
 	&emsp;❄&#65038; <a href="/mrp">国服梦幻拉比最终宝箱出物统计</a><br>
+	❄&#65038; <a target="blank" href="https://github.com/SnowyYANG/PokemonTextBattle">纯文字UI精灵宝可梦在线对战模拟器</a><br>
 	<br>
-	❄&#65038; <a target="blank" href="https://github.com/SnowyYANG/TongLingHime">通灵姬（交互小说，开发中</a><br>
-	❄&#65038; <a href="/dreamcage">雪鸟幻梦之笼（小说</a><br>
+	❄&#65038; <a target="blank" href="https://github.com/SnowyYANG/Delusion">少女躁狂妄想（小说</a><br>
+	❄&#65038; <a >雪鸟幻梦之笼（小说，准备重写，封印中</a><br>
 	❄&#65038; <a target="blank" href="https://store.steampowered.com/app/1511310/_/">消灭魔王军-改（放置策略游戏</a><br>
 </p>
 <p>
