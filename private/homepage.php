ed6860059ed0345e6a9b91f3546af30ba4920a3f
Snowy YANG <snowyyang@outlook.com>
2021-05-26 19:26:17 +0800

    fix target _blank in homepage, add heart of a bird in a cage

--- private/homepage.php
+++ private/homepage.php
@@ -15,11 +15,12 @@ function view() {
 	&emsp;❄&#65038; <a href="/mc">普通染色颜色代码大全</a><br>
 	&emsp;❄&#65038; <a href="/mr">韩服鉴定详细概率</a><br>
 	&emsp;❄&#65038; <a href="/mrp">国服梦幻拉比最终宝箱出货概率统计</a><br>
-	❄&#65038; <a target="blank" href="https://github.com/SnowyYANG/PokemonTextBattle">精灵宝可梦纯文字UI在线对战模拟器</a><br>
+	❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/PokemonTextBattle">精灵宝可梦纯文字UI在线对战模拟器</a><br>
 	<br>
-	❄&#65038; <a target="blank" href="https://github.com/SnowyYANG/Delusions">少女躁狂妄想</a>（短篇小说集<br>
 	❄&#65038; <a href="/love">幻梦情劫</a>（私小说<br>
-	❄&#65038; <a target="blank" href="https://store.steampowered.com/app/1511310/_/">消灭魔王军-改</a>（放置策略游戏<br>
+	❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/Delusions">少女躁狂妄想</a>（短篇小说集<br>
+	❄&#65038; <a target="_blank" href="https://store.steampowered.com/app/1511310/">消灭魔王军-改</a>（放置策略游戏<br>
+	❄&#65038; <a target="_blank" href="https://store.steampowered.com/app/1604930/">笼鸟之心</a>（多结局视觉小说<br>
 </p>
 <p>
 	<a href="/about">关于Snowy</a>
