5e7508bbc4f821a213f89e4b4e991b66937cb2e2
Snowy <snowyyang@outlook.com>
2022-02-07 23:49:18 +0800

    qr code

--- private/homepage.php
+++ private/homepage.php
@@ -13,6 +13,7 @@ function view() {
 	❄&#65038; <a href="/rfwiki">符文工房4中文百科</a><br>
 	❄&#65038; 洛奇Mabinogi<br>
 	&emsp;❄&#65038; <a href="/mc">普通染色颜色代码大全</a><br>
+	&emsp;❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/MabinogiClock">洛奇小闹钟</a><br>
 	&emsp;❄&#65038; <a href="/mr">韩服旧版鉴定详细概率</a><br>
 	&emsp;❄&#65038; <a href="/mrp">国服梦幻拉比最终宝箱出货概率统计</a><br>
 	❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/PokemonBattleSimulator">精灵宝可梦对战模拟器 - 纯文字UI版</a><br>
