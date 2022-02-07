3eea1ed5e3354061712eb7b4ef31c362ed80eb29
Snowy <snowyyang@outlook.com>
2022-02-08 01:07:22 +0800

    proofread

--- private/homepage.php
+++ private/homepage.php
@@ -16,7 +16,7 @@ function view() {
 	&emsp;❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/MabinogiClock">洛奇小闹钟</a><br>
 	&emsp;❄&#65038; <a href="/mr">韩服旧版鉴定详细概率</a><br>
 	&emsp;❄&#65038; <a href="/mrp">国服梦幻拉比最终宝箱出货概率统计</a><br>
-	❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/PokemonBattleSimulator">精灵宝可梦对战模拟器 - 纯文字UI版</a><br>
+	❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/PokemonBattleSimulator">宝可梦对战模拟器 - 纯文字UI版</a><br>
 	<br>
 	❄&#65038; <a target="_blank" href="https://read.douban.com/reader/essay/317897624/">幻梦情劫</a>（私小说<br>
 	❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/Delusions">少女躁狂妄想</a>（短篇小说集<br>
