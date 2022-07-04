f8fa055bb3937c799f7c98b2182cf43d61f0bbfb
Snowy <snowyyang@outlook.com>
2022-07-04 23:39:37 +0800

    mabi pages reorder

--- private/homepage.php
+++ private/homepage.php
@@ -14,8 +14,8 @@ function view() {
 	❄&#65038; 洛奇Mabinogi<br>
 	&emsp;❄&#65038; <a href="/mc">普通染色颜色代码大全</a><br>
 	&emsp;❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/MabinogiClock">洛奇小闹钟</a><br>
-	&emsp;❄&#65038; <a href="/mr">韩服旧版鉴定详细概率</a><br>
 	&emsp;❄&#65038; <a href="/mrp">国服梦幻拉比最终宝箱出货概率统计</a><br>
+	&emsp;❄&#65038; <a href="/mr">韩服旧版鉴定详细概率</a><br>
 	❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/PokemonBattleOnline">Pokemon Battle Online</a>（宝可梦对战平台<br>
 	<br>
 	❄&#65038; <a target="_blank" href="https://github.com/SnowyYANG/Delusions">少女躁狂妄想</a>（架空历史企划<br>
