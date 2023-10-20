8cf89b1f3227fee9237a0c565039164197d0fc8e
Snowy <snowyyang@outlook.com>
2023-10-20 15:45:54 +0800

    center

--- private/homepage.php
+++ private/homepage.php
@@ -8,11 +8,11 @@
 function view() {
 ?>
 <h2>欢迎来到<span style="color:blue"><b>雪亚</b>小站</span></h2>
-<p style="color:red">
+<p style="color:red;text-align:center">
 符文工房4中文百科已转让：<a href="https://rf4wiki.allie.tw" target="_blank">https://rf4wiki.allie.tw</a><br>
 洛奇Mabinogi相关已搬家：<a href="https://luoqi.wiki" target="_blank">https://luoqi.wiki</a>
 </p>
-<p onmouseenter="yin(this)" onmouseleave="yang(this)">八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>经风经雨生不毁 历霜历雪尽妖娆<br>机关算尽不误命 露财露色也平安<br>耐得春宵萧寒夜 平步青云又一朝</p>
+<p style="text-align:center" onmouseenter="yin(this)" onmouseleave="yang(this)">八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>经风经雨生不毁 历霜历雪尽妖娆<br>机关算尽不误命 露财露色也平安<br>耐得春宵萧寒夜 平步青云又一朝</p>
 <script>
 var yin = function(e) {
 	e.innerHTML = '八面玲珑百灵鸟 嘤嘤楚楚笼中傲<br>经枫经雨生不悔 若霜若雪竞妖娆<br>机关算尽不误命 露才露色也平安<br>耐得春宵萧寒夜 平步青云又一朝​';
@@ -22,7 +22,7 @@ var yang = function(e) {
 };
 </script>
 <div>
-	<h2>我的作品</h2>
+	<h3>我的作品</h3>
 	<p>
 	❄&#65038; <a href="/love">幻梦情劫</a>（私小说<br>
 	❄&#65038; <a target="_blank" href="https://store.steampowered.com/app/2116000">阴桃花</a>（互动小说<br>
@@ -31,7 +31,7 @@ var yang = function(e) {
 	</p>
 </div>
 <div>
-	<h2>关于Snowy</h2>
+	<h3>关于Snowy</h3>
 	<p>全职幻想的自由职业者<br>
 	事业感情均已稳定 谢绝各类猎头<br>
 	人生梦想成为全能天才美少女<br>
